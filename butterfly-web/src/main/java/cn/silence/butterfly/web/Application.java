6ce29582e50210e0f95016475aa392ec0bfbde0d
rainofsilence <rainofsilence@foxmail.com>
2023-04-16 23:52:10 +0800

    :tada: Init

---
+++ butterfly-web/src/main/java/cn/silence/butterfly/web/Application.java
@@ -0,0 +1,17 @@
+package cn.silence.butterfly.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rainofsilence
+ * @date 2023/4/16 23:40
+ * @desc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
