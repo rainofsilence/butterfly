c67d32b148a4028f0a334fec15dd86b22a8a5ab8
rainofsilence <rainofsilence@foxmail.com>
2023-04-17 20:23:55 +0800

    :sparkles: Add StringUtils.java
    添加驼峰字符串转下划线字符串

---
+++ butterfly-core/src/main/java/cn/silence/butterfly/core/util/StringUtils.java
@@ -0,0 +1,50 @@
+package cn.silence.butterfly.core.util;
+
+/**
+ * @author rainofsilence
+ * @date 2023/4/17 08:19
+ * @desc
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    /**
+     * 驼峰字符串转下划线字符串
+     *
+     * @param camelStr
+     * @return
+     */
+    public static String camel2Underscore(String camelStr) {
+        return convertCamel(camelStr, '_');
+    }
+
+    /**
+     * 转换驼峰字符串为指定分隔符的字符串<br/>
+     * For example: camelStr:"UserInfo" separator:'_' <br/>
+     * return "user_info"
+     *
+     * @param camelStr
+     * @param separator
+     * @return
+     */
+    public static String convertCamel(String camelStr, char separator) {
+        if (isEmpty((camelStr))) return camelStr;
+        StringBuilder sbOut = new StringBuilder();
+        char[] chars = camelStr.toCharArray();
+        for (int i = 0, len = chars.length; i < len; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                if (i != 0) {
+                    sbOut.append(separator);
+                }
+                sbOut.append(Character.toLowerCase(c));
+                continue;
+            }
+            sbOut.append(c);
+        }
+        return sbOut.toString();
+    }
+}
