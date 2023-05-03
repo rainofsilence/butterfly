0c186df09ba84ed1e85dc829527e732d7d3a0806
rainofsilence <rainofsilence@foxmail.com>
2023-05-03 22:56:31 +0800

    :sparkles: Add EnumGeneratorTest
    添加系统枚举值生成工具类

---
+++ butterfly-web/src/test/java/util/EnumGeneratorTest.java
@@ -0,0 +1,20 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author rainofsilence
+ * @version 1.0.0
+ * @since 2023/05/03 22:53:22
+ */
+public class EnumGeneratorTest {
+
+    /**
+     * 通过 CSV 文件自动生成系统枚举值插入语句
+     */
+    @Test
+    void generateByCSV() {
+        // TODO
+        System.out.println("generateByCSV success");
+    }
+}
